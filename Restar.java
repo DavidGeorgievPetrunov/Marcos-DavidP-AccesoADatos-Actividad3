66f33da7f6a41a08ab8ab28b8a62c57fa2503564
David Georgiev Petrunov <114876785+DavidGeorgievPetrunov@users.noreply.github.com>
2023-10-05 16:53:40 +0200

    Update Restar.java
    Añadidos setters y gettters

--- Restar.java
+++ Restar.java
@@ -8,6 +8,22 @@ public class Restar {
         restarOperacion();
     }
 
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+    
     private void restarOperacion() {
         System.out.println(this.a-this.b);
     }
